fbf41625a95ea745edf6080f9c6985145be73af9
wassupea <62280250+wassupea@users.noreply.github.com>
2021-08-04 13:04:12 +0800

    zzz

--- School/settings.py
+++ School/settings.py
@@ -96,15 +96,15 @@ WSGI_APPLICATION = 'School.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
 
-        #'ENGINE': 'django.db.backends.mysql',
-        #'NAME': 'gms',
-        #'USER': 'gms_user',
-        #'PASSWORD': 'gms_pass',
-        #'HOST': 'localhost',
-        #'PORT': '3306',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gms',
+        'USER': 'gms_user',
+        'PASSWORD': 'gms_pass',
+        'HOST': 'localhost',
+        'PORT': '3306',
 
     }
 }
